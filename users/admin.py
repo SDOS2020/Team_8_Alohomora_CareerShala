25202a2f204dd93f965d9973de9fbf7470686af4
Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>
2020-11-04 17:56:44 +0530

    Add custom user model.

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,66 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.contrib.auth.models import Group
+
+from .models import CustomUser
+
 
 # Register your models here.
+
+# TODO understand all the fields here
+
+class CustomUserCreationForm(forms.ModelForm):
+    password = forms.CharField(label='Password', widget=forms.PasswordInput, required=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'first_name', 'phone_number', 'last_name', 'date_of_birth',)
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+        return user
+
+
+class CustomUserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = CustomUser
+        fields = (
+            'email', 'password', 'first_name', 'last_name', 'phone_number', 'date_of_birth', 'is_active', 'is_admin',)
+
+    def clean_password(self):
+        return self.initial['password']
+
+
+class CustomUserAdmin(UserAdmin):
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+    list_display = ('email', 'password', 'first_name', 'last_name', 'phone_number', 'date_of_birth', 'is_admin')
+    list_filter = ('is_admin',)
+    exclude = ['verified', ]  # TODO is this required if readonly_fields is present?
+    readonly_fields = ('verified',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'phone_number', 'date_of_birth')}),
+        ('Permissions', {'fields': ('is_admin', 'verified')}),
+    )
+    add_fieldsets = (  # TODO why this if we have fieldsets?
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password', 'first_name', 'last_name', 'phone_number', 'date_of_birth',),
+        }),
+    )
+
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ()
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.unregister(Group)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,59 @@
+from django.contrib.auth.models import (BaseUserManager, AbstractBaseUser)
 from django.db import models
 
-# Create your models here.
+from users.methods import generate_token
+
+
+# Refer: https://docs.djangoproject.com/en/3.1/topics/auth/customizing/#a-full-example
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, first_name, last_name, date_of_birth, password=None):
+        if not email:
+            raise ValueError('Email is a required field.')
+        user = self.model(email=self.normalize_email(email),
+                          first_name=first_name,
+                          last_name=last_name,
+                          date_of_birth=date_of_birth)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, first_name, last_name, date_of_birth, password=None):
+        user = self.create_user(email=email, first_name=first_name, last_name=last_name, password=password,
+                                date_of_birth=date_of_birth)
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class CustomUser(AbstractBaseUser):
+    # General fields
+    email = models.EmailField(verbose_name='email address', max_length=255, unique=True)
+    first_name = models.CharField(verbose_name='first name', max_length=255)
+    last_name = models.CharField(verbose_name='last name', max_length=255)
+    phone_number = models.CharField(verbose_name='phone number', max_length=14, unique=True, blank=True, null=True)
+    date_of_birth = models.DateField(verbose_name='date of birth')
+
+    # Fields related to email verification
+    email_verification_token = models.CharField(max_length=32,
+                                                default=generate_token)  # TODO Remove this hardcoded value
+    verified = models.BooleanField(default=False, verbose_name='Verified')
+
+    # Django-specific fields
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    objects = CustomUserManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'date_of_birth']
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
