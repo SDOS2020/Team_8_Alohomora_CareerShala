dff840e8812fa5dae78ad802249c97f2681f90b7
Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>
2020-11-05 13:53:19 +0530

    #7 Add a method in methods.py to send email in a new thread.

--- users/methods.py
+++ users/methods.py
@@ -1,5 +1,14 @@
+import threading
+
+from django.core.mail import send_mail
 from django.utils.crypto import get_random_string
 
 
 def generate_token():
     return get_random_string(length=32)
+
+
+def send_mail_async(sender: str, receivers, subject, body):
+    t = threading.Thread(target=send_mail, args=(subject, body, sender, receivers))
+    t.setDaemon(True)
+    t.start()
