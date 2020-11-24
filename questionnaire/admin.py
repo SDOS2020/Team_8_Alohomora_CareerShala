f66e46c1c6031f118718a231ca26d2d4ec2e14ab
Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>
2020-11-24 16:41:39 +0530

    Add basic models and model-admins for Questionnaire.

--- questionnaire/admin.py
+++ questionnaire/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
 # Register your models here.
+from questionnaire.models import Questionnaire, Question, Option
+
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+    show_change_link = True
+
+
+class OptionInline(admin.TabularInline):
+    model = Option
+    show_change_link = True
+
+
+class QuestionnaireAdmin(admin.ModelAdmin):
+    inlines = [
+        QuestionInline,
+    ]
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [
+        OptionInline
+    ]
+
+
+admin.site.register(Questionnaire, QuestionnaireAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Option)
--- questionnaire/models.py
+++ questionnaire/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
+
 # Create your models here.
+class Questionnaire(models.Model):
+    name = models.CharField(max_length=500)
+    continuation_questionnaire = models.ForeignKey('self', related_name='previous_questionnaire', null=True, blank=True,
+                                                   on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.name
+
+
+class Question(models.Model):
+    body = models.CharField(max_length=1000)
+    questionnaire = models.ForeignKey('Questionnaire', related_name='question', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.body
+
+
+class Option(models.Model):
+    body = models.CharField(max_length=1000)
+    question = models.ForeignKey('Question', related_name='option', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.body
