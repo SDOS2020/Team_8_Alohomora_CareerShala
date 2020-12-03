bf0fc39e1c60c7b57ca3321246f7bd763e92bd59
Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>
2020-12-03 10:56:25 +0000

    Modify settings for migrating to postgresql

--- AlohomoraCareershala/settings.py
+++ AlohomoraCareershala/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 from django.contrib.messages import constants
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -92,12 +93,11 @@ WSGI_APPLICATION = 'AlohomoraCareershala.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+postgresql_db_url = os.environ.get('POSTGRESQL_DB_URL')
+DATABASES['default'] = dj_database_url.config(default=postgresql_db_url)
+DATABASES['default'] = dj_database_url.parse(postgresql_db_url, conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
