ca019801157e6e9f0cdbb96e9f7c5595b58afd1c
Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>
2020-12-03 19:50:12 +0530

    Change static-files related settings.

--- AlohomoraCareershala/settings.py
+++ AlohomoraCareershala/settings.py
@@ -134,12 +134,12 @@ USE_TZ = True
 # STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets/')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'static/'),
 )
 
 # Custom User
