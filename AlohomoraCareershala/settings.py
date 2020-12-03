6dfd1d40dd402e9080ef477fc5d5cc5690a47732
Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>
2020-12-03 09:01:51 +0000

    Change static to staticfiles in settings

--- AlohomoraCareershala/settings.py
+++ AlohomoraCareershala/settings.py
@@ -142,7 +142,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 
 # Custom User
