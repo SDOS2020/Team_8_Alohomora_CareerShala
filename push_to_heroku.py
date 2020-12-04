24bd4b5f6cc07a54b48cf75e9a8c6daaeb2c742c
Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>
2020-12-05 02:53:09 +0530

    Fix push_to_heroku.py script.

--- push_to_heroku.py
+++ push_to_heroku.py
@@ -3,8 +3,10 @@ import os
 if __name__ == '__main__':
     confirm = input("Are you sure to continue? (y/n).")
     if confirm.lower() == "y":
-        os.system("git add --force */migrations/*.py")
-        os.system('git commit -am "Add migrations" ')
+        for directory in os.listdir():
+            if "venv" not in directory:
+                os.system(f"git add --force {directory}/migrations/*.py$")
+        os.system('git commit -am "Add migrations"')
         os.system('git push heroku release-heroku:master --force')
         os.system('git rm --cached */migrations/*.py')
         os.system('git reset --hard HEAD~1')
