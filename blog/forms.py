fdcdc1d57e34947c89c08f80d18c8d8c81d2195b
Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>
2020-12-09 17:33:37 +0530

    Add Post class.

---
+++ blog/forms.py
@@ -0,0 +1,12 @@
+from ckeditor.widgets import CKEditorWidget
+from django import forms
+
+from blog.models import Post
+
+
+class PostCreationForm(forms.ModelForm):
+    body = forms.CharField(widget=CKEditorWidget())
+
+    class Meta:
+        model = Post
+        fields = '__all__'
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
+from ckeditor.fields import RichTextField
 from django.db import models
 
+
 # Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey('users.CustomUser', on_delete=models.CASCADE, related_name='blogs')
+    title = models.CharField(max_length=255)
+    body = RichTextField()
+    likes = models.ManyToManyField('users.CustomUser', blank=True)
+
+    def __str__(self):
+        return f'{self.title}, by {self.author.first_name} {self.author.last_name}'
