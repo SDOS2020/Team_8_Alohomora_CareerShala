b4c4c9963ec7d9fd588351277d693729dd35297c
Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>
2020-12-14 21:20:52 +0530

    Add page to add posts by the expert.

--- blog/admin.py
+++ blog/admin.py
@@ -8,5 +8,11 @@ from blog.models import Post
 class PostAdmin(admin.ModelAdmin):
     form = PostCreationForm
 
+    # https://code.djangoproject.com/wiki/CookBookNewformsAdminAndUser
+    # https://stackoverflow.com/a/855837/5394180
+    def save_model(self, request, obj, form, change):
+        obj.author = request.user
+        obj.save()
+
 
 admin.site.register(Post, PostAdmin)
