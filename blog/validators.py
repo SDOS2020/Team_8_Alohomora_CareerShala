fa37d75ad7c6b9e151fd2f097274ef958d78ee50
Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>
2020-12-16 02:51:44 +0530

    Fix bug in validate_post_type (3).

--- blog/validators.py
+++ blog/validators.py
@@ -5,6 +5,6 @@ from blog.models import Post
 
 
 def validate_post_type(post_type):
-    if (not is_int(post_type)) or (
-            not any(post_type == allowed_post_type[0] for allowed_post_type in Post.POST_TYPE)):
+    if not ((is_int(post_type)) and (
+    any(int(post_type) == allowed_post_type[0] for allowed_post_type in Post.POST_TYPE))):
         raise ValidationError(message="post_type invalid")
