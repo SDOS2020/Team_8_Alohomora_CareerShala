49231039f9f7e81364f08414f67af35acbeab36f
Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>
2021-01-28 00:46:26 +0530

    Add route for admin dashboard.

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('home/', views.home, name='dashboard-home'),
     path('opportunities/', views.opportunities, name='dashboard-opportunities'),  # TODO remove this
     path('courses/', views.courses, name='dashboard-courses'),
+    path('admin/', views.home_admin, name='admin-dashboard'),
 ]
